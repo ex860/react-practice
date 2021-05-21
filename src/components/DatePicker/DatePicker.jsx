45a8206f112f01e8251a1c6cad4c77b2721f8272
YuHsien <ex8600@gmail.com>
2021-05-21 12:26:12 +0800

    Switch DatePicker.jsx to hooks

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,81 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from './Calendar';
 import './DatePicker.scss';
 
-export default class DatePicker extends React.Component {
-    constructor(props) {
-        super(props);
-        const curDateObj = new Date();
-        this.yearOfToday = curDateObj.getFullYear();
-        this.monthOfToday = curDateObj.getMonth() + 1;
-        this.dateOfToday = curDateObj.getDate();
-        this.state = {
-            selectedDay: {
-                year: this.yearOfToday,
-                month: this.monthOfToday,
-                date: this.dateOfToday,
-            },
-            inputDate: '',
-            show: false,
-        };
-    }
-    handleUpdate = (newDay) => {
+const DatePicker = () => {
+    const curDateObj = new Date();
+    const yearOfToday = curDateObj.getFullYear();
+    const monthOfToday = curDateObj.getMonth() + 1;
+    const dateOfToday = curDateObj.getDate();
+    const [selectedDay, setSelectedDay] = useState({
+        year: yearOfToday,
+        month: monthOfToday,
+        date: dateOfToday,
+    });
+    const [inputDate, setInputDate] = useState('');
+    const [show, setShow] = useState(false);
+    const handleUpdate = (newDay) => {
         const { year, month, date } = newDay;
-        this.setState({
-            selectedDay: newDay,
-            inputDate: `${year}-${String(month).padStart(2, '00')}-${String(date).padStart(2, '00')}`,
-            show: false,
-        });
+        setSelectedDay(newDay);
+        setInputDate(`${year}-${String(month).padStart(2, '00')}-${String(date).padStart(2, '00')}`);
+        setShow(false);
     };
-    inputChange = (event) => {
-        const { value } = event.target;
-        this.setState({ inputDate: value });
+    const inputChange = (event) => {
+        setInputDate(event.target.value);
     };
-    onEnterPress = (event) => {
+    const onEnterPress = (event) => {
         if (event.key === 'Enter') {
-            const { inputDate } = this.state;
             const dateObj = new Date(inputDate);
             if (/^\d{4}-\d{2}-\d{2}$/.test(inputDate) && !Number.isNaN(Number(dateObj))) {
-                this.setState({
-                    selectedDay: {
-                        year: Number(inputDate.split('-')[0]),
-                        month: Number(inputDate.split('-')[1]),
-                        date: Number(inputDate.split('-')[2]),
-                    },
-                    show: false,
+                setSelectedDay({
+                    year: Number(inputDate.split('-')[0]),
+                    month: Number(inputDate.split('-')[1]),
+                    date: Number(inputDate.split('-')[2]),
                 });
+                setShow(false);
             }
         }
     };
-    setShow = (newState) => {
-        this.setState({ show: newState });
-    };
-    render() {
-        return (
-            <div className="content">
-                <div className="input-div">
-                    <input
-                        placeholder="YYYY-MM-DD"
-                        value={this.state.inputDate}
-                        onChange={this.inputChange}
-                        onFocus={() => {
-                            this.setShow(true);
-                        }}
-                        onKeyPress={this.onEnterPress}
-                    />
-                </div>
-                {this.state.show ? (
-                    <Calendar
-                        selectedDay={this.state.selectedDay}
-                        updateSelectedDay={this.handleUpdate}
-                        today={{
-                            year: this.yearOfToday,
-                            month: this.monthOfToday,
-                            date: this.dateOfToday,
-                        }}
-                    />
-                ) : null}
+    return (
+        <div className="content">
+            <div className="input-div">
+                <input
+                    placeholder="YYYY-MM-DD"
+                    value={inputDate}
+                    onChange={inputChange}
+                    onFocus={() => {
+                        setShow(true);
+                    }}
+                    onKeyPress={onEnterPress}
+                />
             </div>
-        );
-    }
-}
+            {show ? (
+                <Calendar
+                    selectedDay={selectedDay}
+                    updateSelectedDay={handleUpdate}
+                    today={{
+                        year: yearOfToday,
+                        month: monthOfToday,
+                        date: dateOfToday,
+                    }}
+                />
+            ) : null}
+        </div>
+    );
+};
+export default DatePicker;
